f9a18d6c5902914d249b4eeb3392bb6f1fe7db51
Denis-shl <denish13@live.ru>
2019-12-18 18:47:38 +0300

    fix bag

--- singleton/singleton.go
+++ singleton/singleton.go
@@ -1,9 +1,14 @@
-package singleton
+package Singelton
 
 import (
 	"sync"
 )
 
+// Global variable
+var instance *singleton
+var once sync.Once
+
+//Singleton is an interface
 type Singleton interface {
 	Add()
 	Done()
@@ -15,28 +20,29 @@ type singleton struct {
 	count int
 }
 
-var instance *singleton
-var once sync.Once
-
+// GetInstance object creation
 func GetInstance() Singleton {
-
 	once.Do(func() {
 		instance = new(singleton)
 	})
 	return instance
 }
 
+// Add increase counter by 1
 func (p *singleton) Add() {
 	p.Lock()
 	defer p.Unlock()
 	p.count++
 }
 
+// Done decrement the count
 func (p *singleton) Done() {
 	p.Lock()
 	defer p.Unlock()
 	p.count--
 }
+
+// GetValue receiving count
 func (p *singleton) GetValue() int {
 	return p.count
 }
--- singleton/singleton_test.go
+++ singleton/singleton_test.go
@@ -1,30 +1,28 @@
-package singleton
+package Singelton
 
 import (
-	"fmt"
+	"github.com/stretchr/testify/assert"
 	"sync"
 	"testing"
 )
 
-func TestSingelton(t *testing.T) {
-
-	wg := sync.WaitGroup{}
-	x := GetInstance()
-	for i := 0; i < 100; i++ {
+func TestSingleton(t *testing.T) {
+	t.Run("test  Singleton", func(t *testing.T) {
+		wg := sync.WaitGroup{}
 		x := GetInstance()
-		x.Add()
-		x.Done()
-
-		wg.Add(1)
-		go func(x Singleton, wg *sync.WaitGroup) {
-			defer wg.Done()
-			y := GetInstance()
-			y.Add()
-			if x != y {
-				t.Errorf("got %p want  %p", x, y)
-			}
-		}(x, &wg)
-	}
-	wg.Wait()
-	fmt.Println(x.GetValue())
+		for i := 0; i < 100; i++ {
+			x = GetInstance()
+			x.Add()
+			x.Done()
+			wg.Add(1)
+			go func(x Singleton, wg *sync.WaitGroup) {
+				defer wg.Done()
+				y := GetInstance()
+				y.Add()
+				if !assert.EqualValues(t, x,y, "") {
+					t.Errorf("got %p want  %p", x, y)
+				}
+			}(x, &wg)
+		}
+	})
 }
