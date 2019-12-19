0159b717b02c19e19d9dc20c0d6ab2d826e063d3
Denis-shl <denish13@live.ru>
2019-12-19 09:45:08 +0300

    circle list

---
+++ circularList/circularList.go
@@ -0,0 +1,21 @@
+package hasCycle
+
+func hasCycle(head *ListNode) bool {
+	maps := make(map[*ListNode]int)
+	list := head
+	for {
+		if list == nil {
+			break
+		} else {
+			if maps[list] == 0 {
+				maps[list]++
+			} else {
+				return true
+			}
+		}
+		list = list.Next
+	}
+	return false
+}
+
+
---
+++ circularList/circularList_test.go
@@ -0,0 +1,39 @@
+package hasCycle
+
+import (
+	"testing"
+)
+
+type ListNode struct {
+	Val  int
+	Next *ListNode
+}
+
+func (p *ListNode)Append(head *ListNode, listNew *ListNode)*ListNode{
+	tmp := &head
+	for {
+		if (*tmp).Next == nil{
+			(*tmp).Next = listNew
+			return  head
+		}
+		tmp = &(*tmp).Next
+	}
+}
+
+func TestHasCycle(t *testing.T){
+
+	t.Run("test Cycle list", func(t *testing.T){
+		head := &ListNode{}
+		for i := 0; i < 10; i++ {
+			listNew := &ListNode{}
+			head.Append(head, listNew)
+		}
+		head.Next.Next.Next.Next.Next.Next.Next.Next = head
+		got := hasCycle(head)
+		want := true
+
+		if got != want {
+			t.Errorf("error test got %v  want %v", got, want)
+		}
+	})
+}
