ab9449b64cc0d1f4b832565d225806ac4c776d09
Denis-shl <denish13@live.ru>
2019-12-24 12:51:16 +0300

    memento

---
+++ intersect/intersect_test.go
@@ -0,0 +1,24 @@
+package intersect
+
+import (
+	"reflect"
+	"testing"
+)
+
+const (
+	testIntersect = "test intersect"
+)
+
+func TestIntersect(t *testing.T) {
+	t.Run(testIntersect, func(t *testing.T) {
+		var (
+			nums1 = []int{1, 2, 2, 1}
+			nums2 = []int{2, 2}
+		)
+		want := []int{2, 2}
+		got := intersect(nums1, nums2)
+		if !reflect.DeepEqual(want, got) {
+			t.Errorf("error test intersect want %v got %v", want, got)
+		}
+	})
+}
---
+++ memento/memento_test.go
@@ -0,0 +1,33 @@
+package memento
+
+import (
+	"github.com/stretchr/testify/assert"
+	"reflect"
+	"testing"
+)
+
+const (
+	testNotNil = "Test designers"
+	testBackup = "Test backup"
+)
+
+func TestMemento(t *testing.T) {
+	compA := NewTextEditorA()
+	storage := NewStorage()
+	t.Run(testNotNil, func(t *testing.T) {
+		if !assert.NotNil(t, compA, storage) {
+			t.Errorf("error test not nil want not nil got: compA %v, storage %v", compA, storage)
+		}
+	})
+	t.Run(testBackup, func(t *testing.T) {
+		compA.SetMemento(storage)
+		compA.SetData(12)
+		compA.SetBackup()
+		compA.SetData(134)
+		got := compA.BackBackup()
+		want := 12
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("test error want %v got %v", want, got)
+		}
+	})
+}
