3a06af2d3e6404b355eaef43b3d81c97229a9af6
Denis-shl <denish13@live.ru>
2019-12-28 18:48:10 +0300

    leetcode task

---
+++ pkg/leetcode/DifferentParentheses/differentParentheses.go
@@ -0,0 +1,37 @@
+package diffWaysToCompute
+
+import (
+	"strconv"
+)
+
+func diffWaysToCompute(input string) []int {
+	var (
+		res, res1, res2 []int
+		str1, str2      []rune
+	)
+	str := []rune(input)
+	for i, num := range input {
+		if num == '-' || num == '+' || num == '*' {
+			str1 := append(str1, str[:i]...)
+			str2 := append(str2, str[i+1:]...)
+			res1 = diffWaysToCompute(string(str1))
+			res2 = diffWaysToCompute(string(str2))
+			for _, n1 := range res1 {
+				for _, n2 := range res2 {
+					if num == '-' {
+						res = append(res, n1-n2)
+					} else if num == '+' {
+						res = append(res, n1+n2)
+					} else if num == '*' {
+						res = append(res, n1*n2)
+					}
+				}
+			}
+		}
+	}
+	if res == nil {
+		x, _ := strconv.Atoi(input)
+		res = append(res, x)
+	}
+	return res
+}
---
+++ pkg/leetcode/DifferentParentheses/differentParentheses_test.go
@@ -0,0 +1,33 @@
+package diffWaysToCompute
+
+import (
+	"reflect"
+	"testing"
+)
+
+const (
+	testOne = "test one leetCode task"
+	testTwo = "test two leetCode task"
+)
+
+func TestDiffWaysToCompute(t *testing.T) {
+	var (
+		str1 string = "2-1-1"
+		str2 string = "2*3-4*5"
+	)
+	t.Run(testOne, func(t *testing.T) {
+		want := []int{2, 0}
+		got := diffWaysToCompute(str1)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("error test one want %v got %v", want, got)
+		}
+	})
+
+	t.Run(testTwo, func(t *testing.T) {
+		want := []int{-34, -10, -14, -10, 10}
+		got := diffWaysToCompute(str2)
+		if !reflect.DeepEqual(got, want) {
+			t.Errorf("error test two want %v, got %v", want, got)
+		}
+	})
+}
