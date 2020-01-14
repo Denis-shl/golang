502c1fe1e068b3fd6ede023b368959359f13a911
Denis-shl <denish13@live.ru>
2020-01-14 16:18:39 +0300

    fix

--- cmd/mergetree/main.go
+++ cmd/mergetree/main.go
@@ -8,7 +8,7 @@ import (
 
 func main() {
 	mas := []int{1, 2, 3, 4, 5, 6, 7, 8, 9}
-	obj := merge.NewMerge()
+	obj := merge.NewTreeMerger()
 	mer := merge.NewCreater()
 	creater := mer.SetValue(mas)
 	n := obj.MergeTree(creater, creater)
