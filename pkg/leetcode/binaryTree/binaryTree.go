c5b58964ef6514b2f49f1938cc3b2f417463c630
Denis-shl <denish13@live.ru>
2020-01-14 08:49:57 +0300

    fix

--- pkg/leetcode/binaryTree/binaryTree.go
+++ pkg/leetcode/binaryTree/binaryTree.go
@@ -1,15 +1,15 @@
 package mergeetree
 
-// Handler ...
-type Handler interface {
-	MergeTrees(t1, t2 Creater) Creater
+// Merge ...
+type Merge interface {
+	MergeTree(t1, t2 Creater) Creater
 }
 
-type obj struct {
+type tree struct {
 }
 
-// MergeTrees ...
-func (o *obj) MergeTrees(t1, t2 Creater) Creater {
+// MergeTree ...
+func (o *tree) MergeTree(t1, t2 Creater) Creater {
 	if t1 == nil {
 		return t2
 	}
@@ -22,28 +22,28 @@ func (o *obj) MergeTrees(t1, t2 Creater) Creater {
 	t3.SetVal(t2.GetVal() + t1.GetVal())
 	if t1.GetLeft() != nil {
 		if t2.GetLeft() != nil {
-			t3.SetLeft(o.MergeTrees(t1.GetLeft(), t1.GetRight()))
+			t3.SetLeft(o.MergeTree(t1.GetLeft(), t1.GetRight()))
 		}
 		if t2.GetLeft() == nil {
-			t3.SetLeft(o.MergeTrees(t1.GetLeft(), nil))
+			t3.SetLeft(o.MergeTree(t1.GetLeft(), nil))
 		}
 	} else {
-		t3.SetLeft(o.MergeTrees(nil, t2.GetLeft()))
+		t3.SetLeft(o.MergeTree(nil, t2.GetLeft()))
 	}
 	if t1.GetRight() != nil {
 		if t2.GetRight() != nil {
-			t3.SetRight(o.MergeTrees(t1.GetRight(), t2.GetRight()))
+			t3.SetRight(o.MergeTree(t1.GetRight(), t2.GetRight()))
 		}
 		if t2.GetRight() == nil {
-			t3.SetRight(o.MergeTrees(t1.GetRight(), nil))
+			t3.SetRight(o.MergeTree(t1.GetRight(), nil))
 		}
 	} else {
-		t3.SetRight(o.MergeTrees(nil, t2.GetRight()))
+		t3.SetRight(o.MergeTree(nil, t2.GetRight()))
 	}
 	return t3
 }
 
-// NewHandler ...
-func NewHandler() Handler {
-	return &obj{}
+// NewMerge ...
+func NewMerge() Merge {
+	return &tree{}
 }
